d4a93a3c99e282dc76545439e9092df2647bf79e
Prete <martinprete@gmail.com>
2017-08-25 07:13:31 -0300

    Fix typo.

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const db = mongo.connect(config.mongo.url + config.mongo.db);
 db.then(db => {
     //scrap
     scraper.start(logger, db).then((bulks) => {
-        consoe.log('Insertando en db...');
+        console.log('Insertando en db...');
         //store
         Promise.all([
             //los telegramas y errores se guardan cada 1000 en db
